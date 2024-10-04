434fbbf8d9b651f2d1cf66696f54420a70a20acd
kotan4ik <kostyuk.max@gmail.com>
2024-10-04 18:34:37 +0800

    ref: WebDriverFactory is changed to create headless mode driver

--- src/main/java/com/kotan4ik/webdriver/WebDriverFactory.java
+++ src/main/java/com/kotan4ik/webdriver/WebDriverFactory.java
@@ -2,6 +2,7 @@ package com.kotan4ik.webdriver;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class WebDriverFactory {
   private static final String BROWSER_PROPERTY = "browser";
@@ -11,13 +12,15 @@ public class WebDriverFactory {
   public static WebDriver createWebDriver() {
     Browser browser = getActiveBrowser();
     ConfigLoader configLoader = new ConfigLoader("webdriverconfig.properties");
+    ChromeOptions options = new ChromeOptions();
+    options.addArguments("--headless");
     switch (browser) {
       case CHROME:
         System.setProperty(WEBDRIVER_CHROME_DRIVER_PROPERTY, configLoader.getProperty("chromeDriverPath"));
-        return new ChromeDriver();
+        return new ChromeDriver(options);
       case YANDEX:
         System.setProperty(WEBDRIVER_CHROME_DRIVER_PROPERTY, configLoader.getProperty("yandexDriverPath"));
-        return new ChromeDriver();
+        return new ChromeDriver(options);
       default:
         throw new IllegalArgumentException("Unsupported browser: " + browser);
     }
